ac5383592eb7158413c364bfd448a5f501a075d7
chinmayparekh <chinmayparekh11@gmail.com>
2022-05-26 12:24:41 +0530

    Added Date time methods to be used

--- backend/src/main/java/com/nplab/extension/dashboard/service/AssetDivisionService.java
+++ backend/src/main/java/com/nplab/extension/dashboard/service/AssetDivisionService.java
@@ -1,12 +1,16 @@
 //package com.nplab.extension.dashboard.service;
 //
-//import java.util.ArrayList;
-//import java.util.List;
-//
 //import com.nplab.extension.dao.AssetSummaryDao;
-//import com.nplab.extension.db.RequestCount;
-//import org.joda.time.DateTime;
 //import org.springframework.stereotype.Service;
+//
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.Hashtable;
+//import java.util.List;
+//import java.util.Map;
+//
+//import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
+//import static java.time.temporal.TemporalAdjusters.lastDayOfMonth;
 //
 //@Service
 //public class AssetDivisionService {
@@ -17,8 +21,20 @@
 //    private long externalCasesCount;
 //    private static List<String> sampleTypes = List.of("Tumor", "Nerve", "Muscle",
 //            "Multiple Biopsies", "Epilepsy", "Block", "Slides", "Other");
+////    Hashtable<String, Integer> months = new Hashtable<String, Integer>();
+////    months.put("January",1);
+////    months.put("February",2);
+////    months.put("March",3);
+////    months.put("April",4);
+////    months.put("May",5);
+////    months.put("June",6);
+////    months.put("July",7);
+////    months.put("August",8);
+////    months.put("September",9);
+////    months.put("October",10);
+////    months.put("November",11);
+////    months.put("December",12);
 //
-//    private static List<String> months = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 //
 //    public AssetDivisionService(String to, String from, String specimen, AssetSummaryDao assetSummaryDao) {
 //        this.internalCasesCount = assetSummaryDao.countSamplesByCriteria(
@@ -33,8 +49,10 @@
 //
 //    public static List<Long> findInternalSamples(String from, String to, AssetSummaryDao assetSummaryDao) {
 //        List<Long> sampleCount = new ArrayList<>();
+////        LocalDate initial = LocalDate.of(2014, 2, 13);
+////        LocalDate start = initial.with(firstDayOfMonth());
+////        LocalDate end = initial.with(lastDayOfMonth());
 //        //store the last day of the to month as a DateTime object
-//
 //
 ////        for (int i = 1; i <= days; i++) {
 ////            casesCount.add(new CasesCount(currentTime.minusDays(i - 1).toString().substring(0, 10),
