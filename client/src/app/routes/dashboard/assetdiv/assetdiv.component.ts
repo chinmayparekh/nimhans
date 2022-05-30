3f434467895bd9055dbce90430449ab98d43e62c
Kritinp <pkritin@gmail.com>
2022-05-31 01:14:55 +0530

    Added dynamic x-axis, need to add dropdown for specimen

--- client/src/app/routes/dashboard/assetdiv/assetdiv.component.ts
+++ client/src/app/routes/dashboard/assetdiv/assetdiv.component.ts
@@ -2,31 +2,25 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AssetService } from './assetdiv.service';
 import { DatePipe } from '@angular/common';
 import { TurnAroundTimeService } from '../turn-around-time/turn-around-time.service';
+import {ChartComponent,ApexAxisChartSeries,ApexChart,ApexXAxis,ApexTitleSubtitle} from "ng-apexcharts";
+import { start } from 'repl';
 
-import {
-  ChartComponent,
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexXAxis,
-  ApexTitleSubtitle
-} from "ng-apexcharts";
+export type ChartOptions = {series: ApexAxisChartSeries; chart: ApexChart; xaxis: ApexXAxis; title: ApexTitleSubtitle;};
 
-export type ChartOptions = {
-  series: ApexAxisChartSeries;
-  chart: ApexChart;
-  xaxis: ApexXAxis;
-  title: ApexTitleSubtitle;
-};
 @Component({
   selector: 'app-assetdiv',
   templateUrl: './assetdiv.component.html',
   styleUrls: ['./assetdiv.component.scss']
 })
+
 export class AssetdivComponent implements OnInit {
   startDate:any;
   endDate:any;
-  data1=[];
-  data2 = [];
+  data1=[]; //Internal Samples
+  data2 = []; //External Samples
+  newCategories=[];
+  numMonths : Number;
+  months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]; //Used to get Xaxis
 
   @ViewChild("chart") chart: ChartComponent;
   public chartOptions: Partial<ChartOptions>;
@@ -35,25 +29,62 @@ export class AssetdivComponent implements OnInit {
 
   ngOnInit(): void 
   {
-    
+    //Setting initial date
     this.startDate = new Date();
     this.startDate.setMonth(this.startDate.getMonth() - 6);
     this.startDate = this.datepipe.transform(this.startDate, 'yyyy-MM-dd');
     this.endDate = this.datepipe.transform(new Date(), 'yyyy-MM-dd');
-    this.initializeChart();
-    this.populateChart(this.startDate, this.endDate);
+    
+
+    this.initializeChart();//Initializes chart
 
     //called when new dates are passed
-    this.turnAroundTimeService.dates.subscribe((dates: string[]) => {
+    this.turnAroundTimeService.dates.subscribe((dates: string[]) => 
+    {
       this.startDate = dates[0];
       this.endDate = dates[1];
       this.data1=[];
       this.data2 = [];
-      this.populateChart(this.startDate, this.endDate);
+      this.newCategories=[];
+      this.sendReq(this.startDate, this.endDate); //Sends HTTP GET Request to get Sample Data
+      console.log("cat", this.newCategories);
+      this.populateChart(); // Fills chart with Data fetched previously
     });
 
 
     // console.log("Date", this.startDate);
+  }
+  private sendReq(startd :any , endd : any):void
+  {
+    //HTTP GET Request
+    this.assetService.getJSON(startd, endd).subscribe(data => {
+      console.log(data);
+    this.numMonths = data.length;
+    var startMonth = parseInt(startd.slice(5,7)) -1;
+
+    //Creating separate arrays for internal and external cases and dynamically creating x-axis
+    for(var i=0; i<this.numMonths; i++)
+    {
+      this.data1.push(data[i].internalCasesCount);
+      this.data2.push(data[i].externalCasesCount);
+
+      //Getting X-Axis
+      //newCategories[] is x-axis labels 
+      if( startMonth == 11) //If month is Dec, reset to Jan
+        {
+          this.newCategories.push(this.months[startMonth]);
+          console.log("new pushed", this.newCategories);
+          startMonth=0;
+          continue;
+        }
+        else 
+        {
+          this.newCategories.push(this.months[startMonth]);
+          console.log("new pushed", this.newCategories, " startMonth ", startMonth);
+          startMonth++;
+        }
+    }
+    });
   }
 
     private initializeChart():void
@@ -77,40 +108,39 @@ export class AssetdivComponent implements OnInit {
           text: "Sample Chart"
         },
         xaxis: {
-          categories: ["Jan", "Feb",  "Mar", "Apr", "May", "June"],
-          // categories: [],
+          categories: [],
         }
       };
     }
 
-    private populateChart(startd :any , endd : any):void
+
+    private populateChart():void
     {
-      this.assetService.getJSON(startd, endd).subscribe(data => {
-        console.log(data);
-  
-        //Creating separate arrays for internal and external cases
-        for(var i=0; i<data.length; i++)
-        {
-          this.data1.push(data[i].internalCasesCount);
-          this.data2.push(data[i].externalCasesCount);
-        }
-      });
-  
-      
       console.log("data1 ",this.data1);
       console.log("data2 ",this.data2);
      
-      this.chartOptions.series=
-        [{
-          name: "Internal Samples",
-          // data: [1,2,3,4,5,6],
-          data: this.data1,
+      this.chartOptions = {
+        series: [
+          {
+            name: "Internal Samples",
+            data: this.data1,
+          },
+          {
+            name: "External Samples",
+            data: this.data2,
+          }
+        ],
+        chart: {
+          height: 350,
+          type: "bar",
         },
-        {
-          name: "External Samples",
-          // data: [2,4,6,8,9,1],
-          data:this.data2,
-        }];
+        title: {
+          text: "Sample Chart"
+        },
+        xaxis: {
+          categories: this.newCategories,
+        }
+      };
       
     }
 }
